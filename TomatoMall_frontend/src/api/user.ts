8cdf15e0fd47564d6f5a1519d794ec0b7e0c8752
231250131 <3423928179@qq.com>
2025-03-26 15:56:59 +0800

    zdb部分修改

---
+++ TomatoMall_frontend/src/api/user.ts
@@ -0,0 +1,64 @@
+import {axios} from '../utils/request'
+import {USER_MODULE} from './_prefix'
+
+type LoginInfo = {
+    username: string,
+    password: string
+}
+
+type RegisterInfo = {
+    username: string,
+    password: string,
+    name: string,
+    avatar?: string,
+    role: string,
+    telephone?: string,
+    email?: string,
+    location?: string,
+}
+
+type UpdateInfo = {
+    username: string,
+    password?: string,
+    name?: string,
+    avatar?: string,
+    role?: string,
+    telephone?: string,
+    email?: string,
+    location?: string,
+}
+
+
+// 如果有“Vue: This may be converted to an async function”警告，可以不管
+// 用户登录
+export const userLogin = (loginInfo: LoginInfo) => {
+    return axios.post(`${USER_MODULE}/login`, null, {params: loginInfo})
+        .then(res => {
+            return res
+        })
+}
+
+// 用户注册
+export const userRegister = (registerInfo: RegisterInfo) => {
+    return axios.post(`${USER_MODULE}/register`, registerInfo,
+        {headers: {'Content-Type': 'application/json'}})
+        .then(res => {
+            return res
+        })
+}
+
+// 获取用户信息
+export const userInfo = () => {
+    return axios.get(`${USER_MODULE}`)
+        .then(res => {
+            return res
+        })
+}
+
+// 更新用户信息
+export const userInfoUpdate = (updateInfo: UpdateInfo) => {
+    return axios.post(`${USER_MODULE}`, updateInfo, {headers: {'Content-Type': 'application/json'}})
+        .then(res => {
+            return res
+        })
+}
---
+++ TomatoMall_frontend/src/views/user/Login.vue
@@ -0,0 +1,123 @@
+<script setup lang="ts">
+import {ElForm, ElFormItem, ElMessage} from "element-plus"
+import {ref, computed} from 'vue'
+import {router} from '../../router'
+import {userInfo, userLogin} from "../../api/user.ts"
+
+// 输入框值（需要在前端拦截不合法输入：是否为空+额外规则）
+const username = ref('')
+const password = ref('')
+
+// 电话号码是否为空
+const hasUsernameInput = computed(() => username.value != '');
+// 密码是否为空
+const hasPasswordInput = computed(() => password.value != '')
+// 电话号码的规则
+// 密码不设置特殊规则
+// 登录按钮可用性
+const loginDisabled = computed(() => {
+  return !(hasUsernameInput.value && hasPasswordInput.value); // 修改为 hasUsernameInput
+});
+
+// 登录按钮触发
+function handleLogin() {
+  userLogin({
+    username: username.value,
+    password: password.value
+  }).then(res => {
+    if (res.data.code === '200') {
+      ElMessage({
+        message: "登录成功！",
+        type: 'success',
+        center: true,
+      })
+      const token = res.data.data
+      sessionStorage.setItem('token', token)
+
+      userInfo().then(res => {
+        sessionStorage.setItem('name', res.data.result.name)
+        router.push({path: "/dashboard"})
+      })
+    } else if (res.data.code === '400') {
+      ElMessage({
+        message: res.data.msg,
+        type: 'error',
+        center: true,
+      })
+      password.value = ''
+    }
+  })
+}
+</script>
+
+
+<template>
+  <el-main class="main-frame bgimage">
+    <el-card class="login-card">
+      <div>
+        <h1>登入您的账户</h1>
+        <el-form>
+          <el-form-item>
+            <label v-if="!hasUsernameInput" for="username">用户名</label>
+            <label v-else for="username">用户名</label>
+            <el-input id="username" type="text" v-model="username"
+                      required placeholder="请输入用户名" />
+          </el-form-item>
+
+          <el-form-item>
+            <label for="password">账户密码</label>
+            <el-input id="password" type="password" v-model="password"
+                      required
+                      placeholder="••••••••"/>
+          </el-form-item>
+
+          <span class="button-group">
+              <el-button @click.prevent="handleLogin" :disabled="loginDisabled"
+                         type="primary">登入</el-button>
+              <router-link to="/register" v-slot="{navigate}">
+                <el-button @click="navigate">去注册</el-button>
+              </router-link>
+          </span>
+        </el-form>
+      </div>
+    </el-card>
+  </el-main>
+</template>
+
+
+<style scoped>
+.main-frame {
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.bgimage {
+  background-image: url("../../assets/vue.svg");
+}
+
+.login-card {
+  width: 60%;
+  padding: 10px;
+}
+
+.error-warn {
+  color: red;
+}
+
+.error-warn-input {
+  --el-input-focus-border-color: red;
+}
+
+.button-group {
+  padding-top: 10px;
+  display: flex;
+  flex-direction: row;
+  gap: 30px;
+  align-items: center;
+  justify-content: right;
+}
+</style>
