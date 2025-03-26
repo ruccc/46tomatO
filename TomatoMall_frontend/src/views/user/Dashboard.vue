bc313b00b01ce72d15548d32a7cfe36fb91e4a90
231250131 <3423928179@qq.com>
2025-03-26 20:04:37 +0800

    登陆界面done

--- TomatoMall_frontend/src/views/user/Dashboard.vue
+++ TomatoMall_frontend/src/views/user/Dashboard.vue
@@ -1,11 +1,207 @@
 <script setup lang="ts">
+import { ref, computed } from 'vue';
+import { userInfo, userInfoUpdate } from '../../api/user.ts';
+import { router } from '../../router';
+import { UserFilled } from "@element-plus/icons-vue";
+import { ElMessage, ElMessageBox } from 'element-plus';
 
+const username = ref(''); // 添加 username
+const name = ref('');
+const telephone = ref(''); // 修改为 telephone
+const email = ref(''); // 添加 email
+const location = ref(''); // 修改为 location
+const avatar = ref(''); // 添加 avatar
+
+const newName = ref('');
+const newLocation = ref('');
+
+const displayInfoCard = ref(false);
+
+const password = ref('');
+const confirmPassword = ref('');
+
+const hasConfirmPasswordInput = computed(() => confirmPassword.value != '');
+const isPasswordIdentical = computed(() => password.value == confirmPassword.value);
+const changeDisabled = computed(() => {
+  return !(hasConfirmPasswordInput.value && isPasswordIdentical.value);
+});
+
+getUserInfo();
+
+function getUserInfo() {
+  // 获取 username
+  username.value = sessionStorage.getItem('username') || '';
+
+  userInfo().then(res => {
+    if (res.data.code === '200') {
+      name.value = res.data.data.name;
+      telephone.value = res.data.data.telephone;
+      email.value = res.data.data.email;
+      location.value = res.data.data.location;
+      avatar.value = res.data.data.avatar;
+
+      newName.value = name.value;
+      newLocation.value = location.value;
+    } else {
+      ElMessage.error(res.data.msg);
+    }
+  });
+}
+
+function updateInfo() {
+  userInfoUpdate({
+    username: username.value,
+    name: newName.value,
+    location: newLocation.value,
+    avatar: avatar.value,
+    telephone: telephone.value,
+    email: email.value
+  }).then(res => {
+    if (res.data.code === '200') {
+      ElMessage.success('更新成功！');
+      getUserInfo();
+    } else {
+      ElMessage.error(res.data.msg);
+    }
+  });
+}
+
+function updatePassword() {
+  userInfoUpdate({
+    username: username.value,
+    password: password.value,
+  }).then(res => {
+    if (res.data.code === '200') {
+      password.value = '';
+      confirmPassword.value = '';
+      ElMessageBox.alert(
+          `请重新登录`,
+          '修改成功',
+          {
+            confirmButtonText: '跳转到登录',
+            type: "success",
+          }).then(() => router.push({ path: "/login" }));
+    } else {
+      ElMessage.error(res.data.msg);
+      password.value = '';
+      confirmPassword.value = '';
+    }
+  });
+}
 </script>
 
 <template>
+  <el-main class="main-container">
+    <el-card class="aside-card">
+      <div class="avatar-area">
+        <el-avatar :icon="UserFilled" :size="80"></el-avatar>
+        <span class="avatar-text"> 欢迎您，{{ name }}</span>
+      </div>
 
+      <el-divider></el-divider>
+
+      <el-descriptions :column="1" border title="个人信息">
+        <template #extra>
+          <el-button type="primary" @click="displayInfoCard = !displayInfoCard;">
+            <span v-if="displayInfoCard">修改密码</span>
+            <span v-else>修改个人信息</span>
+          </el-button>
+        </template>
+
+        <el-descriptions-item label="联系电话">
+          {{ telephone }}
+        </el-descriptions-item>
+
+        <el-descriptions-item label="地址">
+          {{ location }}
+        </el-descriptions-item>
+      </el-descriptions>
+    </el-card>
+
+    <el-card v-if="displayInfoCard" class="change-card">
+      <template #header>
+        <div class="card-header">
+          <span>个人信息</span>
+          <el-button @click="updateInfo">更新</el-button>
+        </div>
+      </template>
+
+      <el-form>
+        <el-form-item label="昵称">
+          <el-input v-model="newName" />
+        </el-form-item>
+
+        <el-form-item label="手机号">
+          <el-input v-model="telephone" disabled />
+        </el-form-item>
+
+        <el-form-item label="收货地址">
+          <el-input type="textarea" rows="4" v-model="newLocation" placeholder="中华门" />
+        </el-form-item>
+      </el-form>
+    </el-card>
+
+    <el-card v-else class="change-card">
+      <template #header>
+        <div class="card-header">
+          <span>修改密码</span>
+          <el-button @click="updatePassword" :disabled="changeDisabled">修改</el-button>
+        </div>
+      </template>
+
+      <el-form>
+        <el-form-item label="密码">
+          <el-input type="password" v-model="password" placeholder="•••••••••" />
+        </el-form-item>
+        <el-form-item>
+          <label v-if="!hasConfirmPasswordInput">确认密码</label>
+          <label v-else-if="!isPasswordIdentical" class="error-warn">密码不一致</label>
+          <label v-else>确认密码</label>
+          <el-input type="password" v-model="confirmPassword"
+                    :class="{ 'error-warn-input': !isPasswordIdentical }" placeholder="•••••••••" />
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </el-main>
 </template>
 
 <style scoped>
+.error-warn {
+  color: red;
+}
 
+.error-warn-input {
+  --el-input-focus-border-color: red;
+}
+
+.main-container {
+  display: flex;
+  flex-direction: row;
+  padding: 15px;
+  gap: 5px;
+  justify-content: center;
+}
+
+.card-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.change-card {
+  width: 66%;
+}
+
+.avatar-area {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  gap: 30px;
+}
+
+.avatar-text {
+  font-size: x-large;
+  font-weight: bolder;
+  padding-right: 40px;
+}
 </style>
--- TomatoMall_frontend/src/views/user/Register.vue
+++ TomatoMall_frontend/src/views/user/Register.vue
@@ -1,11 +1,157 @@
 <script setup lang="ts">
+import { ref, computed } from 'vue';
+import { router } from '../../router';
+import { userRegister } from "../../api/user.ts";
+import { ElMessage } from 'element-plus';
 
+// 输入框值
+const username = ref('');
+const password = ref('');
+const confirmPassword = ref('');
+const name = ref('');
+const avatar = ref('');
+const telephone = ref('');
+const email = ref('');
+const location = ref('');
+
+// 电话号码的规则
+const chinaMobileRegex = /^1(3[0-9]|4[579]|5[0-35-9]|6[2567]|7[0-8]|8[0-9]|9[189])\d{8}$/;
+const telLegal = computed(() => chinaMobileRegex.test(telephone.value));
+// 重复密码的规则
+const isPasswordIdentical = computed(() => password.value == confirmPassword.value);
+// 注册按钮可用性
+const registerDisabled = computed(() => {
+  return !(username.value && password.value && name.value &&
+      telLegal.value && isPasswordIdentical.value);
+});
+
+// 注册按钮触发
+function handleRegister() {
+  userRegister({
+    username: username.value,
+    password: password.value,
+    name: name.value,
+    avatar: avatar.value,
+    telephone: telephone.value,
+    email: email.value,
+    location: location.value,
+  }).then(res => {
+    if (res.data.code === '200') {
+      ElMessage({
+        message: "注册成功！请登录账号",
+        type: 'success',
+        center: true,
+      });
+      router.push({ path: "/login" });
+    } else if (res.data.code === '400') {
+      ElMessage({
+        message: res.data.msg,
+        type: 'error',
+        center: true,
+      });
+    }
+  });
+}
 </script>
 
 <template>
+  <el-main class="main-frame bgimage">
+    <el-card class="login-card">
+      <div>
+        <h1>创建一个新的账户</h1>
 
+        <el-form>
+          <el-form-item>
+            <label for="username">用户名</label>
+            <el-input id="username" v-model="username" placeholder="请输入用户名" />
+          </el-form-item>
+
+          <el-form-item>
+            <label for="password">密码</label>
+            <el-input type="password" id="password" v-model="password" placeholder="••••••••" />
+          </el-form-item>
+
+          <el-form-item>
+            <label v-if="!isPasswordIdentical">确认密码</label>
+            <label v-else class="error-warn">密码不一致</label>
+            <el-input type="password" id="confirm-password" v-model="confirmPassword"
+                      :class="{ 'error-warn-input': !isPasswordIdentical }" placeholder="••••••••" />
+          </el-form-item>
+
+          <el-form-item>
+            <label for="name">真实姓名</label>
+            <el-input id="name" v-model="name" placeholder="请输入真实姓名" />
+          </el-form-item>
+
+          <el-form-item>
+            <label for="avatar">头像 URL</label>
+            <el-input id="avatar" v-model="avatar" placeholder="请输入头像 URL" />
+          </el-form-item>
+
+          <el-form-item>
+            <label v-if="!telLegal">手机号</label>
+            <label v-else class="error-warn">手机号不合法</label>
+            <el-input id="telephone" v-model="telephone"
+                      :class="{ 'error-warn-input': !telLegal }" placeholder="请输入手机号" />
+          </el-form-item>
+
+          <el-form-item>
+            <label for="email">邮箱</label>
+            <el-input id="email" v-model="email" placeholder="请输入邮箱" />
+          </el-form-item>
+
+          <el-form-item>
+            <label for="location">位置</label>
+            <el-input id="location" v-model="location" placeholder="请输入位置" />
+          </el-form-item>
+
+          <span class="button-group">
+                        <el-button @click.prevent="handleRegister" :disabled="registerDisabled"
+                                   type="primary">创建账户</el-button>
+
+                        <router-link to="/login" v-slot="{ navigate }">
+                            <el-button @click="navigate">去登录</el-button>
+                        </router-link>
+                    </span>
+        </el-form>
+      </div>
+    </el-card>
+  </el-main>
 </template>
 
 <style scoped>
+.main-frame {
+  width: 100%;
+  height: 100%;
 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.bgimage {
+  background-image: url("../../assets/vue.svg");
+}
+
+.login-card {
+  width: 60%;
+  padding: 10px;
+}
+
+.error-warn {
+  color: red;
+}
+
+.error-warn-input {
+  --el-input-focus-border-color: red;
+}
+
+.button-group {
+  padding-top: 10px;
+  display: flex;
+  flex-direction: row;
+  gap: 30px;
+  align-items: center;
+  justify-content: right;
+}
 </style>
